a65d64abf3beef65b2b2989fddd9d54f8ae8b9c8
Goldy Tanjung Wijaya <goldy.tanjung@ui.ac.id>
2023-07-25 22:04:04 +0700

    rapi2

--- components/BookingJadwalContent/index.js
+++ components/BookingJadwalContent/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-// import ChooseBooking from "./ChooseBooking";
 import DetailBooking from "./DetailBooking";
 import { Card, Input, Radio, Select } from "antd";
 import "@amir04lm26/react-modern-calendar-date-picker/lib/DatePicker.css";
